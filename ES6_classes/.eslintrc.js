e0eba7203c89f25b1a272567ce04fb7a05fda362
lucyann <lucyann78@gmail.com>
2025-03-30 17:14:59 -0400

    ES6 Classes

--- ES6_classes/.eslintrc.js
+++ ES6_classes/.eslintrc.js
@@ -1,14 +1,38 @@
 module.exports = {
     env: {
-        browser: true,
-        es2021: true,
+      browser: false,
+      es6: true,
+      jest: true,
     },
-    extends: 'eslint:recommended',
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
     parserOptions: {
-        ecmaVersion: 12,
-        sourceType: 'module',
+      ecmaVersion: 2018,
+      sourceType: 'module',
     },
+    plugins: ['jest'],
     rules: {
-        // Aquí puedes agregar tus reglas personalizadas
+      'max-classes-per-file': 'off',
+      'no-underscore-dangle': 'off',
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
     },
-};
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+  };
+  

7167af763022df23256a1b61a3ef85eeece32a0d
lucyann <lucyann78@gmail.com>
2025-03-30 19:23:35 -0400

    0-test

--- ES6_classes/__tests__/0-classroom.test.js
+++ ES6_classes/__tests__/0-classroom.test.js
@@ -1,4 +1,15 @@
-test('description of the test', () => {
-    // Your test code here
+// 0-classroom.test.js
+import ClassRoom from './0-classroom.js';
+
+describe('ClassRoom', () => {
+    test('should create a ClassRoom instance with correct maxStudentsSize', () => {
+        const room = new ClassRoom(10);
+        expect(room._maxStudentsSize).toBe(10);
+    });
+
+    test('should create a ClassRoom instance with different maxStudentsSize', () => {
+        const room = new ClassRoom(20);
+        expect(room._maxStudentsSize).toBe(20);
+    });
 });
 

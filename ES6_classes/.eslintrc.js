cd9999c544c321424eb75cab888abff4282b5cf9
lucyann <lucyann78@gmail.com>
2025-03-30 16:18:39 -0400

    ES6 Classes Config and Setup

---
+++ ES6_classes/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    rules: {
+        // Aquí puedes agregar tus reglas personalizadas
+    },
+};

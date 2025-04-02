5aec13bb2165cfa796b4a522ccd4e70f91a20852
lucyann <lucyann78@gmail.com>
2025-04-02 16:36:26 -0400

    Basic list of objects eslint corrected

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,20 @@
 export default function getListStudents() {
-    const myArr = [
-      {
-        firstName: 'Guillaume',
-        id: 1,
-        location: 'San Francisco',
-      },
-      {
-        firstName: 'James',
-        id: 2,
-        location: 'Columbia',
-      },
-      {
-        firstName: 'Serena',
-        id: 5,
-        location: 'San Francisco',
-      },
-    ];
-  
-    return myArr;
-  }
+  const myArr = [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
+  return myArr;
+}

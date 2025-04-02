950f5355d4ae16ef4fcc33d9b8caafa00a5c0e57
lucyann <lucyann78@gmail.com>
2025-04-02 19:38:54 -0400

    Filter

---
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,7 @@
+export default function getStudentsByLocation(getStudentList, city) {
+  function filterOut(obj) {
+    if (obj.location === city) return true;
+    return false;
+  }
+  return getStudentList.filter(filterOut);
+}

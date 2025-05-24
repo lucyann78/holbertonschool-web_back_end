2b1cf12c39e018b510991342ce4f7f15ce93e080
Lucy Ann Santiago Lizasoain <lucyannsantiagolizasoain@Lucys-MacBook-Air.local>
2025-05-24 09:53:21 -0400

    Create a more complex HTTP server using Express

---
+++ Node_JS_basic/7-http_express.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+// Get the database file from command line arguments
+const args = process.argv.slice(2);
+
+app.get('/', (req, res) => {
+  res.status(200).type('text/plain').send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.write('This is the list of our students\n');
+
+  if (!args[0]) {
+    // No database file provided
+    res.end('Cannot load the database');
+    return;
+  }
+
+  try {
+    const messages = await countStudents(args[0]);
+    res.end(messages.join('\n'));
+  } catch (err) {
+    res.end(err.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+module.exports = app;
